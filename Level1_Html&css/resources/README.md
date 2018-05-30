28e24b1d5157e5c6fed61360606067a7aed63c60
Dmitry Ivanov <dmitry.ivanov.od@gmail.com>
2018-05-30 23:00:59 +0300

    :tada: initial commit

---
+++ Level1_Html&css/resources/README.md
@@ -0,0 +1,3 @@
+# Hot Dog! It's A Website.
+
+__Task:__ Turn this basic mockup into a web page. This is your shot to show us what you've got while making a site loosely based on Columbus' best hot dogs. 
---
+++ Level2_Javascript/script.js
@@ -0,0 +1,13 @@
+// item example
+// {
+//   name: 'Name', 
+//   description: 'Description', 
+//   expirationDate: '01-30-1999' 
+// }
+
+const filterByExpiration = (items) => {
+  // change this function to return only current items
+  // where expirationDate > today's date
+
+  return items;
+};
