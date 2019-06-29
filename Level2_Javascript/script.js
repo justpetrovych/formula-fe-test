4499eff730295ea356543a7863ca97ebce00f9a6
Natalia Zhydejkina <nata.iiinx88@gmail.com>
2019-06-29 18:46:30 +0300

    add two blocks

--- Level2_Javascript/script.js
+++ Level2_Javascript/script.js
@@ -1,13 +1,16 @@
 // item example
 // {
-//   name: 'Name', 
-//   description: 'Description', 
-//   expirationDate: '01-30-1999' 
+//   name: 'Name',
+//   description: 'Description',
+//   expirationDate: '01-30-1999'
 // }
 
-const filterByExpiration = (items) => {
+const filterByExpiration = items => {
   // change this function to return only current items
   // where expirationDate > today's date
-
-  return items;
+  let currentDate = Date.now();
+  return items.filter(item => {
+    let expirationDate = new Date(item.expirationDate);
+    return expirationDate.getTime() > currentDate;
+  });
 };
